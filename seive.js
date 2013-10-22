a17c028925e9b208133bcbf8dce2150228ac8090
Jon Ramsey <jonathon.ramsey@gmail.com>
2013-10-22 13:58:08 +0100

    Stepping through marking composites manually. Shrunk numbers a bit.

--- seive.js
+++ seive.js
@@ -1,66 +1,48 @@
+/*jshint maxstatements:30 */
 (function($) {
   // calculate
   var ns;
-  var ops;
-  var multiplications;
 
   var max = function(n) {
-    return Math.max(2, Math.floor(Math.sqrt(n)));
-  };
-
-  var op = function(n, op, p) {
-    var o = { n: n, o: op };
-    if (p) { o.p = p; }
-    ops.push(o);
+    return Math.floor(Math.sqrt(n));
   };
 
   var recordPrime = function(n) {
-    ns[n] = "p";
-    op(n, "p");
+    ns[n] = true;
   };
 
-  var recordComposite = function(n, p) {
-    if (! ns[n]) {
-      ns[n] = "c";
-      op(n, "c", p);
-    }
-  };
-
-  var isComposite = function(n) {
-    return ns[n] === "c";
+  var recordComposite = function(n) {
+    if (n !== false) { ns[n] = false; }
   };
 
   var nextPrime = function(p, n) {
     var i;
     for (i = p + 1; i <= n; i += 1) {
-      if (! isComposite(i)) { return i; }
+      if (ns[i] !== false) { return i; }
     }
   };
 
   var seive = function(n) {
     var p = 2;
     var i;
-    ns = [];//Array(n);
-    ops = [];
-    multiplications = 0;
+    ns = [];
     while (p <= max(n)) {
       recordPrime(p);
-      // for (i = 2 * p; i <= n; i += p) {
       for (i = p * p; i <= n; i += p) {
         recordComposite(i, p);
-        multiplications += 1;
       }
       p = nextPrime(p, n);
     }
     do {
       recordPrime(p);
-    } while (p = nextPrime(p, n));
+      p = nextPrime(p, n);
+    } while (p);
   };
 
   var primes = function() {
     var i, l = ns.length, primes = [];
     for (i = 2; i <= l; i += 1) {
-      if (ns[i] === "p") { primes.push(i); }
+      if (ns[i] === true) { primes.push(i); }
     }
     return primes;
   };
@@ -73,11 +55,31 @@
   };
 
   var header = function(n) {
-    var text = "n = " + n + ", so we calculate multiples of the primes that are less than or equal to " + max(n);
+    var text = "<i>n</i> = " + n + ", so we mark multiples of the primes <i>p</i>" +
+      " ≤ &radic;(" + n + ") as composites";
     if (max(n) === 2) {
       text = "n = " + n + ", so we need only consider 2";
     }
     container.append("<h2>" + text);
+  };
+
+  var operationDiv = $("<div id='operation'>");
+
+  var appendOperationDiv = function() {
+    container.append(operationDiv);
+  };
+
+  var writePrimes = function() {
+    operationDiv.empty();
+    var p = primes();
+    var text = "The ";
+    if (p.length > 1) {
+      text += p.length + " primes up to " + (ns.length - 1) + " are ";
+    } else {
+      text += "only prime up to 2 is ";
+    }
+    text += p.join(", ");
+    operationDiv.append(text + ".");
   };
 
   var writeIntegers = function(n) {
@@ -99,83 +101,76 @@
 
   var r, g, b;
 
-  var bgDifference = function(max) {
-    return Math.round((4/max) * (255 - 183));
-  };
-
-  var resetCompositeBG = function(max) {
+  var resetCompositeBG = function() {
     r = 188;
-    g = 255 + bgDifference(max);
-    b = 255 + bgDifference(max);
+    g = 255;
+    b = 255;
   };
 
   var darkenCompositeBG = function(max) {
-    g -= bgDifference(max);
+    var difference = Math.round((4/max) * (255 - 188));
+    r -= difference;
+    g -= difference;
     b = g;
   };
 
   var markComposite = function(n, factorisation) {
-    integerDiv(n).
+    integerDiv(n).not(".composite").
       attr("title", factorisation).
       addClass("composite").
       css("backgroundColor", "rgb(" + r + "," + g + "," + b + ")");
   };
 
-  var writePrimes = function() {
-    var text = "The ";
-    if (primes().length > 1) {
-      text += primes().length + " primes up to " + (ns.length - 1) + " are ";
-    } else {
-      text += "only prime up to 2 is ";
+  var markMultiplesOf = function(p, n) {
+    var i;
+    for (i = p * p; i <= n; i += p) {
+      markComposite(i, p + "×" + Math.round(i/p));
     }
-    text += primes().join(", ");
-    container.append("<div class='result'><p>" + text + "</p></div>");
+    darkenCompositeBG(max(n));
   };
 
-  var animate = true;
-  var animationDelay = $("#delay").val();
-  var animationInterval;
-  var stopAnimation = function() {
-    if (animationInterval) {
-      clearInterval(animationInterval);
-      animationInterval = null;
+  var markRemainingPrimes = function() {
+    primes().forEach(function(p) {
+      markPrime(p);
+    });
+  };
+
+  var writeNextOperation = function(n, p) {
+    operationDiv.empty();
+    var text;
+    var button = $("<button>");
+    if (p <= max(n)) {
+      text = (p === 2) ? "The first" : "The next";
+      text += " prime number <i>p</i> = " + p + ", so we mark multiples of  " +
+        p + ", starting with <i>p<sup>2</sup></i> = " + Math.pow(p, 2) +
+        ", as composites";
+      button.text("Mark multiples of " + p);
+      button.click(function() {
+        markMultiplesOf(p, n);
+        writeNextOperation(n, nextPrime(p, n));
+      });
+      markPrime(p);
+    } else {
+      text = "The next prime <i>p</i> = " + p + " &gt; &radic;(" + n +
+        "), so we have finished. All remaining unmarked integers are primes";
+      button.text("Mark remaining primes");
+      button.click(function() {
+        markRemainingPrimes();
+        writePrimes();
+      });
     }
+    operationDiv.append(text, button);
   };
 
   var displaySeive = function(n) {
-    var frame = 0;
-    var nextFrame = function() {
-      var op = ops[frame];
-      switch (op.o) {
-      case "p":
-        markPrime(op.n);
-        darkenCompositeBG(max(n));
-        break;
-      case "c":
-        markComposite(op.n, op.p + "×" + Math.round(op.n/op.p));
-        break;
-      }
-      if (frame < (ops.length - 1)) {
-        frame += 1;
-      } else {
-        stopAnimation();
-        writePrimes(n);
-      }
-    };
     if (n < 2) { return; }
-    stopAnimation();
     clear();
+    resetCompositeBG();
     header(n);
+    appendOperationDiv();
     writeIntegers(n);
     seive(n);
-    resetCompositeBG(max(n));
-    if (animate) {
-      nextFrame();
-      animationInterval = setInterval(nextFrame, animationDelay);
-    } else {
-      while (frame < (ops.length - 1)) { nextFrame(); }
-      nextFrame();
-    }
+    writeNextOperation(n, 2);
   };
 
   $("#set-n").submit(function(event) {
@@ -186,26 +181,6 @@
   $("#n").change(function(event) {
     event.preventDefault();
     displaySeive(this.value);
-  });
-
-  $("#speed").mousedown(function(event) {
-    stopAnimation();
-  });
-
-  $("#speed").mouseup(function(event) {
-    var value = this.max - this.value;
-    if (value === 0) {
-      animate = false;
-    } else {
-      animate = true;
-      animationDelay = value;
-      console.log(value);
-    }
-    displaySeive($("#n").val());
-  });
-
-  $("#restart").click(function(event) {
-    displaySeive($("#n").val());
   });
 
   displaySeive(100);
