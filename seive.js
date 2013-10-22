8079b734afcdb2f4f6f3d4d116c35103109e96be
Jon Ramsey <jonathon.ramsey@gmail.com>
2013-10-22 13:56:59 +0100

    Second attempt, animated marking, slooow.

--- seive.js
+++ seive.js
@@ -1,112 +1,212 @@
 (function($) {
-  var additions = 0;
-  var container = $("#js-seive");
-
-  var clear = function() {
-    additions = 0;
-    container.html("");
-  };
+  // calculate
+  var ns;
+  var ops;
+  var multiplications;
 
   var max = function(n) {
     return Math.max(2, Math.floor(Math.sqrt(n)));
   };
 
-  var writeHeader = function(n) {
-    var text = "n = " + n + ", so we consider primes ≤ " + max(n);
+  var op = function(n, op, p) {
+    var o = { n: n, o: op };
+    if (p) { o.p = p; }
+    ops.push(o);
+  };
+
+  var recordPrime = function(n) {
+    ns[n] = "p";
+    op(n, "p");
+  };
+
+  var recordComposite = function(n, p) {
+    if (! ns[n]) {
+      ns[n] = "c";
+      op(n, "c", p);
+    }
+  };
+
+  var isComposite = function(n) {
+    return ns[n] === "c";
+  };
+
+  var nextPrime = function(p, n) {
+    var i;
+    for (i = p + 1; i <= n; i += 1) {
+      if (! isComposite(i)) { return i; }
+    }
+  };
+
+  var seive = function(n) {
+    var p = 2;
+    var i;
+    ns = [];//Array(n);
+    ops = [];
+    multiplications = 0;
+    while (p <= max(n)) {
+      recordPrime(p);
+      // for (i = 2 * p; i <= n; i += p) {
+      for (i = p * p; i <= n; i += p) {
+        recordComposite(i, p);
+        multiplications += 1;
+      }
+      p = nextPrime(p, n);
+    }
+    do {
+      recordPrime(p);
+    } while (p = nextPrime(p, n));
+  };
+
+  var primes = function() {
+    var i, l = ns.length, primes = [];
+    for (i = 2; i <= l; i += 1) {
+      if (ns[i] === "p") { primes.push(i); }
+    }
+    return primes;
+  };
+
+  // display
+  var container = $("#seive");
+
+  var clear = function() {
+    container.html("");
+  };
+
+  var header = function(n) {
+    var text = "n = " + n + ", so we calculate multiples of the primes that are less than or equal to " + max(n);
     if (max(n) === 2) {
       text = "n = " + n + ", so we need only consider 2";
     }
     container.append("<h2>" + text);
   };
 
+  var writeIntegers = function(n) {
+    var div = $("<div class='integers'>");
+    for (var i = 1; i <= n; i += 1) {
+      div.append($("<div class='integer'>").attr("id", "n" + i).text(i));
+    }
+    div.find("#n1").text("");
+    container.append(div);
+  };
+
   var integerDiv = function(n) {
     return $("#n" + n);
   };
 
-  var writeIntegers = function(n) {
-    var wrapper = $("<div class='integers'>");
-    for (var i = 1; i <= n; i++) {
-      wrapper.append($("<div class='integer'>").attr("id", "n" + i).text(i));
-    }
-    wrapper.find("#n1").text("");
-    container.append(wrapper);
-  };
-
-  var prime = function(n) {
+  var markPrime = function(n) {
     integerDiv(n).addClass("prime");
   };
 
-  var mustBePrime = function(n) {
-    integerDiv(n).addClass("mustBePrime");
+  var r, g, b;
+
+  var bgDifference = function(max) {
+    return Math.round((4/max) * (255 - 183));
   };
 
-  var composite = function(n) {
-    integerDiv(n).addClass("composite");
-    additions += 1;
+  var resetCompositeBG = function(max) {
+    r = 188;
+    g = 255 + bgDifference(max);
+    b = 255 + bgDifference(max);
   };
 
-  var isComposite = function(n) {
-    return integerDiv(n).hasClass("composite");
+  var darkenCompositeBG = function(max) {
+    g -= bgDifference(max);
+    b = g;
   };
 
-  var isPrime = function(n) {
-    return integerDiv(n).hasClass("prime") || integerDiv(n).hasClass("mustBePrime");
-  }
+  var markComposite = function(n, factorisation) {
+    integerDiv(n).
+      attr("title", factorisation).
+      addClass("composite").
+      css("backgroundColor", "rgb(" + r + "," + g + "," + b + ")");
+  };
 
-  var findNextPrime = function(p, n) {
-    var i;
-    for (i = p + 1; i <= n; i++) {
-      if (! isComposite(i)) {
-        return i;
-      }
+  var writePrimes = function() {
+    var text = "The ";
+    if (primes().length > 1) {
+      text += primes().length + " primes up to " + (ns.length - 1) + " are ";
+    } else {
+      text += "only prime up to 2 is ";
+    }
+    text += primes().join(", ");
+    container.append("<div class='result'><p>" + text + "</p></div>");
+  };
+
+  var animate = true;
+  var animationDelay = $("#delay").val();
+  var animationInterval;
+  var stopAnimation = function() {
+    if (animationInterval) {
+      clearInterval(animationInterval);
+      animationInterval = null;
     }
   };
 
-  var seive = function(n) {
-    var p = 2;
-    while (p <= max(n)) {
-      prime(p);
-      for (i = 2 * p; i <= n; i += p) {
-        composite(i);
+  var displaySeive = function(n) {
+    var frame = 0;
+    var nextFrame = function() {
+      var op = ops[frame];
+      switch (op.o) {
+      case "p":
+        markPrime(op.n);
+        darkenCompositeBG(max(n));
+        break;
+      case "c":
+        markComposite(op.n, op.p + "×" + Math.round(op.n/op.p));
+        break;
       }
-      p = findNextPrime(p, n);
-    }
-    do {
-      mustBePrime(p);
-    } while (p = findNextPrime(p, n));
-  };
-
-  var writePrimes = function(n) {
-    var primes = [];
-    for (i = 2; i <= n; i++) {
-      if (isPrime(i)) {
-        primes.push(i);
+      if (frame < (ops.length - 1)) {
+        frame += 1;
+      } else {
+        stopAnimation();
+        writePrimes(n);
       }
-    }
-    container.append("<div class='result'>So the " + primes.length +
-                     " primes up to " + n + " are " + primes.join(", ") +
-                     ". This required " + additions + " addition" +
-                     (additions === 1 ? "" : "s") + " to determine composites.");
-  };
-
-  var display = function(n) {
-    if (n < 2) return;
+    };
+    if (n < 2) { return; }
+    stopAnimation();
     clear();
-    writeHeader(n);
+    header(n);
     writeIntegers(n);
     seive(n);
-    writePrimes(n);
+    resetCompositeBG(max(n));
+    if (animate) {
+      nextFrame();
+      animationInterval = setInterval(nextFrame, animationDelay);
+    } else {
+      while (frame < (ops.length - 1)) { nextFrame(); }
+      nextFrame();
+    }
   };
 
-  $("#js-set-n").submit(function(event) {
+  $("#set-n").submit(function(event) {
     event.preventDefault();
     event.stopImmediatePropagation();
   });
 
-  $("#js-n").change(function(event) {
+  $("#n").change(function(event) {
     event.preventDefault();
-    display(this.value);
+    displaySeive(this.value);
   });
 
-  display(100);
+  $("#speed").mousedown(function(event) {
+    stopAnimation();
+  });
+
+  $("#speed").mouseup(function(event) {
+    var value = this.max - this.value;
+    if (value === 0) {
+      animate = false;
+    } else {
+      animate = true;
+      animationDelay = value;
+      console.log(value);
+    }
+    displaySeive($("#n").val());
+  });
+
+  $("#restart").click(function(event) {
+    displaySeive($("#n").val());
+  });
+
+  displaySeive(100);
 }(jQuery));
